4aa528a12d955053a97bab0439dff982be4db9b1
badrbin <124630341+badrbin@users.noreply.github.com>
2026-01-16 16:14:00 -0500

    Replicate erpnext_arabic_layout structure exactly

--- manus/setup.py
+++ manus/setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+install_requires = f.read().strip().split("\n")
+
+# get version from __version__ variable in manus/__init__.py
+from manus import __version__ as version
+
 setup(
-    name='manus',
-    version='0.0.1',
-    description='Custom integrations and validations for ERPNext 15',
-    author='Manus',
-    author_email='info@manus.im',
-    packages=find_packages(),
-    zip_safe=False,
-    include_package_data=True,
-    install_requires=[],
+name="manus",
+version=version,
+description="Custom integrations and validations for ERPNext 15",
+author="Manus",
+author_email="info@manus.im",
+packages=find_packages(),
+zip_safe=False,
+include_package_data=True,
+install_requires=install_requires
 )
